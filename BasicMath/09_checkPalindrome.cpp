adaf6a2a04b2e1990d7426312557d97a5aa750bd
DivithV25 <divithv25@gmail.com>
2024-09-13 10:53:32 +0530

    LeetCode-09 Check Palindrome

--- BasicMath/09_checkPalindrome.cpp
+++ BasicMath/09_checkPalindrome.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-           string str1=to_string(x);
-           return str1==string(str1.rbegin(),str1.rend());
-
+           string str1=to_string(x);//converts the integer into a string
+           return str1==string(str1.rbegin(),str1.rend());//creates a reverse string and compares it with original string
     }
 };
