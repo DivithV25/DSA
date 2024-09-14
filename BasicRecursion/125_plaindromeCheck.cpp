28b55e71961dad1f955d0183d258d89b6a80a169
DivithV25 <divithv25@gmail.com>
2024-09-14 22:09:38 +0530

    LeetCode125 Valid Plaindrome

--- BasicRecursion/125_plaindromeCheck.cpp
+++ BasicRecursion/125_plaindromeCheck.cpp
@@ -1,14 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int start = 0;
-        int end = s.size() - 1;
-        while (start <= end) {
-            if (!isalnum(s[start])) {
+    bool isPalindrome(string s) {  
+        int start = 0;                       //TWO POINTERS APPROACH
+        int end = s.size() - 1;                      
+        while (start <= end) { 
+            if (!isalnum(s[start])) {        //CHECKS FOR ALPHANUMERIC VALUE AND INCREMENT THE START POINTER  
                 start++;
-                continue;
+                continue;                    //SKIP TO THE IMEDIATE ITERATION 
             }
-            if (!isalnum(s[end])) {
+            if (!isalnum(s[end])) {             
                 end--;
                 continue;
             }
