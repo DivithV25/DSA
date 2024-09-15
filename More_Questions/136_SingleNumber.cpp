8af445dfa0f8737e7400e0df70d9c62d9515f711
DivithV25 <divithv25@gmail.com>
2024-09-15 21:39:54 +0530

    LeetCode136 Single Number

--- More_Questions/136_SingleNumber.cpp
+++ More_Questions/136_SingleNumber.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> a;
+        unordered_map<int,int> a;                  //STORING ALL THE ELEMENTS FREQUENCY IN MAPS 
         for(auto x:nums){
             a[x]++;
         }
-        for(auto y:a){
+        for(auto y:a){                             //CHOOSING THE ELEMENT WITH FREQUENCY ONE
             if(y.second==1){
             return y.first;
             }
