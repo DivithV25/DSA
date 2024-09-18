b69ae083cad3383d87b78a6deadd062b3f52b240
DivithV25 <divithv25@gmail.com>
2024-09-18 11:55:26 +0530

    Pair Sort MyComparator

--- Additional_concepts/comparator/PairSort.cpp
+++ Additional_concepts/comparator/PairSort.cpp
@@ -1,16 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool mycomparator(pair<int,int> a,pair<int,int> b){
-   if(a.first==b.first){
-    return a.second>b.second;
-   }
-  return a.first>b.first;
+bool mycomparator(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first == b.first)
+    {
+        return a.second > b.second;
+    }
+    return a.first > b.first;
 }
 
-int main(){
-   vector<pair<int,int>> arr={{1,3},{4,3},{2,7}};
-   sort(arr.begin(),arr.end(),mycomparator);
-   for(auto i:arr){
-    cout<< i.first<<":"<<i.second<<" ";
-   }
+int main()
+{
+    vector<pair<int, int>> arr = {{1, 3}, {4, 3}, {2, 7}};
+    sort(arr.begin(), arr.end(), mycomparator);
+    for (auto i : arr)
+    {
+        cout << i.first << ":" << i.second << " ";
+    }
 }
