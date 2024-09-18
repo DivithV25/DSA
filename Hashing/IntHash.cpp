40966d1c7a26d72eae4468fed2f92b6669ce1064
DivithV25 <divithv25@gmail.com>
2024-09-18 19:50:26 +0530

    Concept Array Hashing

---
+++ Hashing/IntHash.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[4]={1,2,3,1};
+    int hash[4]={0};
+    for(int i=0;i<4;i++){
+      hash[arr[i]]=hash[arr[i]]+1;
+    }
+    cout<<hash[1];
+}
