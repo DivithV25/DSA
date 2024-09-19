15e806bb4ab6c0dcf43b953c78a05ac4584dda11
DivithV25 <divithv25@gmail.com>
2024-09-19 11:48:57 +0530

    Bubble Sort

--- Sorting_Techniques/BubbleSort.cpp
+++ Sorting_Techniques/BubbleSort.cpp
@@ -5,13 +5,17 @@ void bubblesort(int arr[], int n)
 {
     for (int i = n - 1; i >= 1; i--)
     { // Move from o to n-1
+    int didswap=0;
         for (int j = 0; j <= i - 1; j++)
         { // j ends at second last elment becasuse you compare with j+1
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                didswap=1;
             }
         }
+        if(didswap==0)
+         break;
     }
 }
 int main()
@@ -25,3 +29,5 @@ int main()
     }
     return 0;
 }
+//TIME COMPLEXITY BEST->O(N), AVERAGE , WORST ->O(N^2)
+//COMPARE AND  SWAP EACH TIME LARGREST ELEMENT TO THE RIGHT SIDE AND LEAVE THAT INDEX TO THE NEXT ITERATIONN
