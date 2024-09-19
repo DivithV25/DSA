2d0a787574add5c31ed7bd0ed8b3bd18bf9c5c60
DivithV25 <divithv25@gmail.com>
2024-09-19 11:42:28 +0530

    Selection Sort

--- Sorting_Techniques/SelectionSort.cpp
+++ Sorting_Techniques/SelectionSort.cpp
@@ -5,21 +5,23 @@ int main()
     int min;
     int n = 5;
     int arr[n] = {3, 2, 5, 1, 4};
-    for (int i = 0; i <= n - 2; i++)
+    for (int i = 0; i <= n - 2; i++) // Swap the minimum element with the first index of the silding array you go only till n-2 because last element have to ntgh to comapre with
     {
         min = i;
-        for (int j = i; j <= n-1 ; j++)
+        for (int j = i; j <= n - 1; j++)
         {
             if (arr[j] < arr[min])
             {
                 min = j;
             }
         }
-        swap(arr[i],arr[min]);
+        swap(arr[i], arr[min]);
     }
-    for(auto i:arr){
-        cout<<i<<" ";
+    for (auto i : arr)
+    {
+        cout << i << " ";
     }
 }
 
-//Time Complexity O(n^2)
+// Time Complexity O(n^2)
+//Minimum element to the left and leave that index for the next iteration
