cd245bcca9c653552300ebc8044c2adc5f4be453
DivithV25 <divithv25@gmail.com>
2024-09-26 13:27:48 +0530

    LeetCode729 My Calender I

---
+++ More_Questions/729_MyCalender.cpp
@@ -0,0 +1,29 @@
+class MyCalendar
+{
+public:
+    vector<pair<int, int>> slot;
+    MyCalendar()
+    {
+    }
+
+    bool book(int start, int end)
+    {
+        for (auto &i : slot)
+        {
+
+            if (!(end <= i.first || start >= i.second))
+            {
+                return false;
+            }
+        }
+
+        slot.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
