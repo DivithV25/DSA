88baeb3cd2920db4cdf06a8c6902ff148f7b99d3
DivithV25 <divithv25@gmail.com>
2024-10-11 17:26:01 +0530

    LeetCode1942 The Number of the Smallest Unoccupied Chair

--- More_Questions/1942_UnoccupidedChair.cpp
+++ More_Questions/1942_UnoccupidedChair.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    typedef pair<int, int> P;
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        vector<int> chairs(n, -1);
-        int TargetArraivalTime = times[targetFriend][0];
-        sort(times.begin(),times.end());
-        for (auto time : times) {
-            int arrival = time[0];
-            int departure = time[1];
-            for (int i = 0; i < n; i++) {
-                if (chairs[i] <= arrival) {
-                    chairs[i] = departure;
-
-                    if (arrival == TargetArraivalTime) {
-                        return i;
-                    }
-                    break;
+        int chair = 0;
+        int targetFriendArrival = times[targetFriend][0];
+        priority_queue<P, vector<P>, greater<P>>
+            occupied; //{departure,chairNo}
+        priority_queue<int, vector<int>, greater<int>> free; //{chairNo}
+        sort(times.begin(), times.end());
+        for (int i = 0; i < n; i++) {
+            int arrivaltime = times[i][0];
+            int departuretime = times[i][1];
+            while (!occupied.empty() && occupied.top().first <= arrivaltime) {
+                free.push(occupied.top().second);
+                occupied.pop();
+            }
+            if (free.empty()) {
+                occupied.push({departuretime, chair});
+                if (arrivaltime == targetFriendArrival)
+                    return chair;
+                chair++;
+            } else {
+                if (arrivaltime == targetFriendArrival) {
+                    return free.top();
                 }
+                occupied.push({departuretime, free.top()});
+                free.pop();
             }
         }
         return -1;
