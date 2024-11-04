d37e08272e430bd3c2ef677fcfa411601e76ce12
DivithV25 <divithv25@gmail.com>
2024-11-04 18:11:53 +0530

    Ineger Custom Sort

--- Additional_concepts/comparator/IntegerSort.cpp
+++ Additional_concepts/comparator/IntegerSort.cpp
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool mycomparator(int a,int b){
-    if(a>b) return true;
+bool mycomparator(int a, int b)
+{
+    if (a > b)
+        return true;
     return false;
 }
 
-int main(){
-   vector<int> arr={1,4,3,2,7};
-   sort(arr.begin(),arr.end(),mycomparator);
-
-
+int main()
+{
+    vector<int> arr = {1, 4, 3, 2, 7};
+    sort(arr.begin(), arr.end(), mycomparator);
 }
