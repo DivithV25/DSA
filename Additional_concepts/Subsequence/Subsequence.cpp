41ca53638abbaa7e61de866495e2fd7afd6ea873
DivithV25 <divithv25@gmail.com>
2024-11-04 18:09:35 +0530

    Subsequence

--- Additional_concepts/Subsequence/Subsequence.cpp
+++ Additional_concepts/Subsequence/Subsequence.cpp
@@ -1,25 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void printF(int ind,vector<int> &ds,int arr[],int n){
-if(ind==n)
+void printF(int ind, vector<int> &ds, int arr[], int n)
 {
-    for(auto it:ds){
-        cout<<it<<" ";
+    if (ind == n)
+    {
+        for (auto it : ds)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+        return;
     }
-    cout<<endl;
-    return;
-}
-//take this index
- ds.push_back(arr[ind]);
- printF(ind+1,ds,arr,n);
-//dont take this index
- ds.pop_back();
- printF(ind+1,ds,arr,n);
+    // take this index
+    ds.push_back(arr[ind]);
+    printF(ind + 1, ds, arr, n);
+    // dont take this index
+    ds.pop_back();
+    printF(ind + 1, ds, arr, n);
 }
 
-int main(){
-    int arr[]={3,1,2};
-    int n=3;
+int main()
+{
+    int arr[] = {3, 1, 2};
+    int n = 3;
     vector<int> ds;
-    printF(0,ds,arr,n);
+    printF(0, ds, arr, n);
 }
