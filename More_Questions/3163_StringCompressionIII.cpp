e241261945288493f3b3bbee134be0158c9e3fe6
DivithV25 <divithv25@gmail.com>
2024-11-04 17:11:09 +0530

    LeetCode3163  String Compression III

---
+++ More_Questions/3163_StringCompressionIII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int n=word.length();
+        int i=0;
+        string result;
+        while(i<n){
+            int count=0;
+            char ch=word[i];
+            while(i<n && count<9 && ch==word[i]){
+                count++;
+                i++;
+            }    
+        result+=to_string(count)+ch;
+
+        }
+        return result;
+    }
+};
