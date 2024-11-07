24de6bbf05c94b9d12b01dade899a16285eaa773
DivithV25 <divithv25@gmail.com>
2024-11-07 18:51:20 +0530

    LeetCode869 Lemonade Change

---
+++ More_Questions/869_LemonadeChange.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    bool lemonadeChange(vector<int> &bills)
+    {
+
+        int five = 0, ten = 0;
+        for (auto bill : bills)
+        {
+            if (bill == 5)
+            {
+                five++;
+            }
+            else if (bill == 10)
+            {
+                if (five)
+                {
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (five && ten)
+                {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3)
+                {
+                    five -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
