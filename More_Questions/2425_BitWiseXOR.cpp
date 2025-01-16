f5d1848c7b4ea725c98e9a2b8866ee8ff66906f8
DivithV25 <divithv25@gmail.com>
2025-01-16 09:05:05 +0530

    LeetCode2425. Bitwise XOR of All Pairings

---
+++ More_Questions/2425_BitWiseXOR.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        if (n % 2 == 0 && m % 2 == 0) {
+            return 0;
+        }
+        int ans = 0;
+        if (n % 2 != 0) {
+            for (auto num : nums2) {
+                ans ^= num;
+            }
+        }
+        if (m % 2 != 0) {
+            for (auto num : nums1) {
+                ans ^= num;
+            }
+        }
+        return ans;
+    }
+};
