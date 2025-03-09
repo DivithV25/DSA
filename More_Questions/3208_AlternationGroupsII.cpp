e42df0243752217b2da1d0d77f6ea890b9be5e8b
DivithV25 <divithv25@gmail.com>
2025-03-09 18:05:48 +0530

    LeetCode3208. Alternating Groups II

---
+++ More_Questions/3208_AlternationGroupsII.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int numberOfAlternatingGroups(vector<int>& colors, int k) {
+            int maxLen = 1, result = 0, n = colors.size();
+            for (int i = 1; i <= n + k - 2; i++) {
+                if (colors[i % n] != colors[(i - 1 + n) % n]) {
+                    maxLen++;
+                } else {
+                    maxLen = 1;
+                }
+                if (maxLen >= k)
+                    result++;
+            }
+            return result;
+        }
+    };
