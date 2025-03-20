92901844fd1870e525451c53d2753299574fc8ba
DivithV25 <divithv25@gmail.com>
2025-03-20 19:07:12 +0530

    Binary To Decimal

--- Additional_concepts/BitManipulation/decimaltobinary.cpp
+++ Additional_concepts/BitManipulation/decimaltobinary.cpp
@@ -14,3 +14,18 @@ string convert(int n){
     reverse(res.begin(),res.end());
     return res;
 }
+
+
+//right shift new number formula
+//n>>k = (n/2^k)
+
+
+//when computer wants to store an negative number, it stores the 2's complement of the number
+
+
+//left shift new number formula 
+//n<<k = n*2^k
+
+
+//NOT OPERATOR ~n
+//if first we flip it if its negative then do 1's complimant or else we dont do one complement
